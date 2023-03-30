097e89add7645692e770fcdb689cafa8e0ea8de6
Cankar001 <Info.Can-Karka@Web.de>
2023-03-30 02:28:16 +0200

    Added basic main code structure

--- src/Main.py
+++ src/Main.py
@@ -2,8 +2,25 @@ import GoogleIAMFunctions
 import GoogleResourceManagerFunctions
 import Logger
 
+def print_features():
+    Logger.info('         Command              |           Description')
+    Logger.info('google_create_project         | Create a new Google cloud project')
+    Logger.info('google_create_service_account | Create a new service account, attached to a project')
+
 def main():
     Logger.info('Starting demo...')
+
+    while True:
+        print_features()
+        cmd = input('Command  > ')
+        cmd = cmd.lower()
+
+        if cmd == 'quit' or cmd == 'q':
+            break
+        elif cmd == 'google_create_project':
+            Logger.info('Creating google project...')
+        elif cmd == 'google_create_service_account':
+            Logger.info('Creating service account...')
 
 if __name__ == '__main__':
     main()
